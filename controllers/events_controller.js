265b011055e31e5fd0e257527076ab30861521e4
Chase Jones <chaseejones@icloud.com>
2022-11-16 20:26:49 -0500

    refactor: delete routes for forms
    Delete routes for form views because they will be implemented in the front end.

--- controllers/events_controller.js
+++ controllers/events_controller.js
@@ -11,31 +11,10 @@ events.get("/", async (req, res) => {
   });
 });
 
-// SHOW NEW FORM FOR EVENTS
-events.get("/new", async (req, res) => {
-  res.status(200).json({
-    message: "Shows new form for events",
-  });
-});
-
 // CREATE NEW EVENT
 events.post("/", async (req, res) => {
   res.status(200).json({
     message: "Create new event",
-  });
-});
-
-// SHOW EDIT FORM FOR EVENT
-events.get("/:id/edit", async (req, res) => {
-  res.status(200).json({
-    message: `Shows edit form for event with id: ${req.params.id}`,
-  });
-});
-
-// SHOW DELETE FORM FOR EVENT WITH :ID
-events.get("/:id/delete", async (req, res) => {
-  res.status(200).json({
-    message: `Show delete form for event with id: ${req.params.id}`,
   });
 });
 

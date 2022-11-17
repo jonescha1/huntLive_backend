7186a36857c35f3e460e9df9da14242de7e79e92
Chase Jones <chaseejones@icloud.com>
2022-11-16 22:09:12 -0500

    feat: update routes to interact with database
    The user can now interact with the database to create, read, update and delete users

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,42 +1,74 @@
 //  DEPENDENCIES
 const { Op } = require("sequelize");
 const users = require("express").Router();
-// const db = require("../models");
-// const { User } = db;
+const db = require("../models");
+const { User } = db;
 
 // SHOW ALL USERS
 users.get("/", async (req, res) => {
-  res.status(200).json({
-    message: "Show all users",
-  });
+  try {
+    const foundUsers = await User.findAll();
+    res.status(200).json(foundUsers);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // CREATE NEW USER
 users.post("/", async (req, res) => {
-  res.status(200).json({
-    message: "Create new user",
-  });
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).json({
+      message: "Successfully created a new user",
+      data: newUser,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 users
   .route("/:id")
   // SHOW USER BY ID
   .get(async (req, res) => {
-    res.status(200).json({
-      message: `Shows user with id: ${req.params.id}`,
-    });
+    try {
+      const foundUser = await User.findOne({
+        where: { id: req.params.id },
+      });
+      res.status(200).json(foundUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   })
   // UPDATE USER BY ID
   .put(async (req, res) => {
-    res.status(200).json({
-      message: `Update route for user with id: ${req.params.id}`,
-    });
+    try {
+      const updateUser = await User.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json({
+        message: `Successfully updated user`,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   })
   // DELETE USER BY ID
   .delete(async (req, res) => {
-    res.status(200).json({
-      message: `Delete user with id: ${req.params.id}`,
-    });
+    try {
+      const deleteUser = await User.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json({
+        message: `Successfully deleted user`,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
 
 // EXPORT
